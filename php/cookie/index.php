20aa960d0b8afdf46505d9ebceba810cd904703e
mingchungou <mingchungou@gmail.com>
2017-05-11 12:37:42 -0600

    Remove font libs

--- php/cookie/index.php
+++ php/cookie/index.php
@@ -11,7 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
 
-        <link rel="stylesheet" href="../../css/bootstrap.min.css"  />
+        <link rel="stylesheet" href="../../css/bootstrap.min.css" />
     </head>
     <body>
         <h1>Cookies</h1>
--- php/cookie/text.php
+++ php/cookie/text.php
@@ -15,7 +15,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
 
-        <link rel="stylesheet" href="../../css/bootstrap.min.css"  />
+        <link rel="stylesheet" href="../../css/bootstrap.min.css" />
         <style media="screen">
             p {
                 font-size: <?php echo $size; ?>
